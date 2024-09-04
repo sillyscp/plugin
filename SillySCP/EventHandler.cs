f9540278b15ef51e94d5546e1ad87fcc98f396cf
Jay <me@jayxtq.xyz>
2024-09-04 17:14:05 +0100

    feat: discord bot client

--- SillySCP/EventHandler.cs
+++ SillySCP/EventHandler.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Interop;
 using AdminToys;
+using Discord;
+using Discord.WebSocket;
 using Exiled.API.Features;
 using PlayerStatsSystem;
 using PluginAPI.Core.Attributes;
@@ -40,6 +43,65 @@ namespace SillySCP
         {
             var playerStats = Plugin.Instance.PlayerStats.OrderByDescending((p) => p.Damage).ToList();
             Map.Broadcast(10, "MVP for this round is " + playerStats[0].Player.Nickname + " with " + playerStats[0].Damage + " damage!");
+        }
+        
+        [PluginEvent(ServerEventType.PlayerJoined)]
+        void OnPlayerJoined(Player player)
+        {
+            var textChannel = GetChannel();
+            var message = GetMessage(textChannel);
+            var messageEmbed = message.Embeds.FirstOrDefault();
+            if (messageEmbed == null)
+            {
+                var embedBuilder = new EmbedBuilder()
+                    .WithTitle("Silly SCP Member List")
+                    .WithColor(Color.Blue)
+                    .WithDescription("- " + player.Nickname);
+                textChannel.ModifyMessageAsync(message.Id, msg => msg.Embed = embedBuilder.Build()).GetAwaiter().GetResult();
+            }
+            else
+            {
+                var embedBuilder = new EmbedBuilder()
+                    .WithTitle("Silly SCP Member List")
+                    .WithColor(Color.Blue)
+                    .WithDescription(messageEmbed.Description + "- " + player.Nickname + "\n");
+                textChannel.ModifyMessageAsync(message.Id, msg =>
+                {
+                    msg.Embed = embedBuilder.Build();
+                    msg.Content = "";
+                }).GetAwaiter().GetResult();
+            }
+            Plugin.Client.SetCustomStatusAsync(Server.PlayerCount + "/30 players active").GetAwaiter().GetResult();
+        }
+
+        [PluginEvent(ServerEventType.PlayerLeft)]
+        void OnPlayerLeft(Player player)
+        {
+            var textChannel = GetChannel();
+            var message = GetMessage(textChannel);
+            var messageEmbed = message.Embeds.FirstOrDefault();
+            if (messageEmbed == null) return;
+            var embedBuilder = new EmbedBuilder()
+                .WithTitle("Silly SCP Member List")
+                .WithColor(Color.Blue)
+                .WithDescription(messageEmbed.Description.Replace("- " + player.Nickname, "").Replace("\n\n", "\n"));
+            textChannel.ModifyMessageAsync(message.Id, msg => msg.Embed = embedBuilder.Build()).GetAwaiter().GetResult();
+            Plugin.Client.SetCustomStatusAsync(Server.PlayerCount + "/30 players active").GetAwaiter().GetResult();
+        }
+
+        private SocketTextChannel GetChannel()
+        {
+            var channel = Plugin.Client.GetChannel(1279544677334253610);
+            if (channel.GetChannelType() != ChannelType.Text) return null;
+            var textChannel = (SocketTextChannel) channel;
+            return textChannel;
+        }
+
+        private IMessage GetMessage(SocketTextChannel channel)
+        {
+            var message = channel.GetMessageAsync(1280910252325339311).GetAwaiter().GetResult();
+            if (message.Author.Id != Plugin.Client.CurrentUser.Id) return null;
+            return message;
         }
     }
 }
--- SillySCP/Plugin.cs
+++ SillySCP/Plugin.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 using Exiled.API.Features;
-using Exiled.Events;
 using PluginAPI.Events;
-using Player = PluginAPI.Core.Player;
 
 namespace SillySCP
 {
@@ -11,12 +12,14 @@ namespace SillySCP
         public static Plugin Instance;
         public List<PlayerStat> PlayerStats;
         private EventHandler handler;
+        public static DiscordSocketClient Client;
 
         public override void OnEnabled()
         {
             Instance = this;
             handler = new EventHandler();
             EventManager.RegisterEvents(this, handler);
+            Task.Run(StartClient);
             base.OnEnabled();
         }
         
@@ -24,7 +27,28 @@ namespace SillySCP
         {
             EventManager.UnregisterEvents(this, handler);
             handler = null;
+            Task.Run(StopClient);
             base.OnDisabled();
+        }
+        
+        private static Task Log(LogMessage msg)
+        {
+            PluginAPI.Core.Log.Info(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private static async Task StartClient()
+        {
+            Client = new DiscordSocketClient();
+            Client.Log += Log;
+            await Client.LoginAsync(TokenType.Bot, Instance.Config.Token);
+            await Client.StartAsync();
+        }
+        
+        private static async Task StopClient()
+        {
+            await Client.StopAsync();
+            await Client.LogoutAsync();
         }
     }
 }
