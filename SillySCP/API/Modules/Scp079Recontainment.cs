d223f67703ef34ed369f08a8a409d0cde3b32ac4
Jay <me@jayxtq.xyz>
2024-12-08 22:08:28 +0000

    fix(SCP079Recontainment): overcharge perm locking doors

--- SillySCP/API/Modules/Scp079Recontainment.cs
+++ SillySCP/API/Modules/Scp079Recontainment.cs
@@ -1,4 +1,6 @@
 ﻿using Exiled.API.Features;
+using MapGeneration.Distributors;
+using PlayerRoles.PlayableScps.Scp079;
 
 namespace SillySCP.API.Modules
 {
@@ -6,7 +8,13 @@ namespace SillySCP.API.Modules
     {
         public static void Recontain()
         {
-            Recontainer.BeginOvercharge();
+            Recontainer.Base.SetContainmentDoors(true, true);
+            Recontainer.Base._alreadyRecontained = true;
+            Recontainer.Base._recontainLater = 3f;
+            foreach (Scp079Generator generator in Scp079Recontainer.AllGenerators)
+            {
+                generator.Engaged = true;
+            }
         }
     }
 }
