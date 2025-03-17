76c5dbed0ef52f613b795cb1dd871ba339424eb5
blankochan <liz@blanky.dev>
2025-03-17 10:52:23 -0500

    fix: make volunteer request a separate command

---
+++ SillySCP/Commands/RequestHuman.cs
@@ -0,0 +1,40 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using SillySCP.API.Features;
+
+
+namespace SillySCP.Commands
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public class RequestHuman : ICommand
+    {
+        public string Command { get; } = "RequestHuman";
+
+        public string[] Aliases { get; } = ["r","rh","request"];
+
+        public string Description { get; } = "request to change into a human, if you're an SCP."; // no clue what the description for this should actually be tbh
+
+        public bool Execute(
+            ArraySegment<string> arguments,
+            ICommandSender sender,
+            out string response
+        )
+        {
+            if (!Player.TryGet(sender, out Player player))
+            {
+                response = "Only Players can use this command.";
+                return false; 
+            }
+
+            if (!player.IsScp)
+            {
+                response = "Only SCP's can use this command.";
+                return false;
+            }
+            
+            response = "Opening a volunteer request."; // I really do not know what terminology should be used, always sounds so weird
+            VolunteerSystem.NewVolunteer(player.Role,original:player);
+            return true;
+        }
+    }
+}
