1356e0303d3674dcc6976dd7cc85ab5aff20f21b
blankochan <liz@blanky.dev>
2025-03-17 10:52:23 -0500

    fix: make volunteer request a separate command

--- SillySCP/Commands/RequestHuman.cs
+++ SillySCP/Commands/RequestHuman.cs
@@ -32,6 +32,12 @@ namespace SillySCP.Commands
                 return false;
             }
             
+            if (player.Role != RoleTypeId.Scp0492) // change if we allow other SCPs to request volunteer swap thingy (i should really figure out that terminology I hate how inconsistent i am)
+            {
+                response = "Only Zombies can use this command.";
+                return false;
+            }
+            
             response = "Opening a volunteer request."; // I really do not know what terminology should be used, always sounds so weird
             VolunteerSystem.NewVolunteer(player.Role,original:player);
             return true;
