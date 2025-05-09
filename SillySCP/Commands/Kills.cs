20a21650489931eb299d766c972b71512f7150fa
Azzy <29222142+AzzyIsNotHere@users.noreply.github.com>
2025-05-09 07:34:43 -0600

    revert: oops i didnt read the issue
    reverts 80eaeef1bace46b41149da6f53e17d68b5d151cc

--- SillySCP/Commands/Kills.cs
+++ SillySCP/Commands/Kills.cs
@@ -5,13 +5,13 @@ using SillySCP.API.Features;
 namespace SillySCP.Commands
 {
     [CommandHandler(typeof(ClientCommandHandler))]
-    public class Stats : ICommand
+    public class Kills : ICommand
     {
-        public string Command { get; } = "stats";
+        public string Command { get; } = "kills";
 
-        public string[] Aliases { get; } = new [] { "k", "s", "kills" };
+        public string[] Aliases { get; } = new [] { "k" };
 
-        public string Description { get; } = "Get your stats for this round.";
+        public string Description { get; } = "Get the amount of kills you have.";
 
         public bool Execute(
             ArraySegment<string> arguments,
@@ -30,18 +30,18 @@ namespace SillySCP.Commands
             if (player.DoNotTrack)
             {
                 response =
-                    "You have do not track enabled, disable it so we can start tracking your stats!";
+                    "You have do not track enabled, disable it so we can start tracking your kills!";
                 return false;
             }
 
             PlayerStat playerStat = Plugin.Instance.PlayerStats.Find((p) => p.Player == player);
             if (playerStat == null)
             {
-                response = "You have no stats to show!";
+                response = "You have 0 kills!";
                 return true;
             }
 
-            response = "You have " + (playerStat.Kills ?? 0) + " player kills, " + (playerStat.ScpKills ?? 0) + " SCP kills, and " + (playerStat.Damage ?? 0) + " damage!";
+            response = "You have " + (playerStat.Kills ?? 0) + " player kills and " + (playerStat.ScpKills ?? 0) + " SCP kills!";
             return true;
         }
     }
