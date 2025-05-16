ec59741ba267b49c9303735e4c2f0bc61cd051d9
Lumi <me@jxtq.moe>
2025-05-16 23:01:38 +0100

    feat: special weapons custom blocks

---
+++ SillySCP/API/Features/SpecialWeaponsPrimitive.cs
@@ -0,0 +1,32 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Features.Toys;
+using SillySCP.API.Components;
+using UnityEngine;
+
+namespace SillySCP.API.Features
+{
+    public class SpecialWeaponsPrimitive
+    {
+        public SpecialWeaponsPrimitive(Vector3 localPos, Room room, Vector3 size)
+        {
+            LocalPosition = localPos;
+            Room = room;
+            Size = size;
+        }
+
+        private Vector3 LocalPosition { get; set; }
+        
+        private Vector3 Size { get; set; }
+        
+        private Room Room { get; set; }
+
+        public void Spawn()
+        {
+            Primitive primitive = Primitive.Create(PrimitiveType.Cube, Room.WorldPosition(LocalPosition), Room.Rotation.eulerAngles, Size);
+            primitive.Collidable = false;
+            primitive.GameObject.AddComponent<CheckVoid>();
+            BoxCollider collider = primitive.GameObject.AddComponent<BoxCollider>();
+            collider.isTrigger = true;
+        }
+    }
+}
---
+++ SillySCP/Handlers/MapHandler.cs
@@ -0,0 +1,39 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using MapGeneration;
+using MEC;
+using SillySCP.API.Features;
+using SillySCP.API.Interfaces;
+using UnityEngine;
+
+namespace SillySCP.Handlers
+{
+    public class MapHandler : IRegisterable
+    {
+        public void Init()
+        {
+            RoomIdentifier.OnAdded += RoomAdded;
+        }
+
+        public void Unregister()
+        {
+            RoomIdentifier.OnAdded -= RoomAdded;
+        }
+
+        private static void RoomAdded(RoomIdentifier r)
+        {
+            // me when exiled doesn't get the rooms here, so have to do a small delay :3
+            Timing.CallDelayed(0, () => OnRoomAdded(r));
+        }
+
+        private static void OnRoomAdded(RoomIdentifier r)
+        {
+            Room room = Room.Get(r);
+            if (r.name.ToLower().Contains("HCZ_Straight_PipeRoom".ToLower()))
+            {
+                SpecialWeaponsPrimitive primitive = new (new (0, 7f, -4.8f), room, new (14f, 3, 3.9f));
+                primitive.Spawn();
+            }
+        }
+    }
+}
