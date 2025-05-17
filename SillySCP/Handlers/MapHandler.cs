062e10b8a82988bfeb8818f9eba53fae5f6c3a87
Lumi <me@jxtq.moe>
2025-05-17 13:40:08 +0100

    refactor: switch statement

--- SillySCP/Handlers/MapHandler.cs
+++ SillySCP/Handlers/MapHandler.cs
@@ -34,30 +34,35 @@ namespace SillySCP.Handlers
                 SpecialWeaponsPrimitive primitive = new (new (0, 7f, -4.8f), room, new (14f, 3, 3.9f));
                 primitive.Spawn();
             }
-            if (room.Type == RoomType.Hcz127)
+            // ReSharper disable once SwitchStatementMissingSomeEnumCasesNoDefault
+            switch (room.Type)
             {
-                // the spot with the ladder
-                SpecialWeaponsPrimitive ladderPrimitive = new(new(-5.75f, 0, 5.77f), room, new(3, 1, 7.5f), new(0, 45, 0));
-                ladderPrimitive.Spawn();
+                case RoomType.Hcz127:
+                {
+                    // the spot with the ladder
+                    SpecialWeaponsPrimitive ladderPrimitive = new(new(-5.75f, 0, 5.77f), room, new(3, 1, 7.5f), new(0, 45, 0));
+                    ladderPrimitive.Spawn();
                 
-                // the spot without the ladder
-                SpecialWeaponsPrimitive primitive = new(new (-5.34f, 0, -6.52f), room, new(5, 1, 3), new(0, 45, 0));
-                primitive.Spawn();
-            }
+                    // the spot without the ladder
+                    SpecialWeaponsPrimitive primitive = new(new (-5.34f, 0, -6.52f), room, new(5, 1, 3), new(0, 45, 0));
+                    primitive.Spawn();
+                    break;
+                }
+                case RoomType.Surface:
+                {
+                    SpecialWeaponsPrimitive primitive = new(new(21.60f, 1.5f, -22.74f), room, new(26, 1, 12));
+                    primitive.Spawn();
 
-            if (room.Type == RoomType.Surface)
-            {
-                SpecialWeaponsPrimitive primitive = new(new(21.60f, 1.5f, -22.74f), room, new(26, 1, 12));
-                primitive.Spawn();
-
-                SpecialWeaponsPrimitive pitPrimitive = new(new(0, -2, -7), room, new(23, 1, 12));
-                pitPrimitive.Spawn();
-            }
-
-            if (room.Type == RoomType.Hcz079)
-            {
-                SpecialWeaponsPrimitive primitive = new(new(-5.40f, -3.5f, -13.5f), room, new(4.5f, 1, 6));
-                primitive.Spawn();
+                    SpecialWeaponsPrimitive pitPrimitive = new(new(0, -2, -7), room, new(23, 1, 12));
+                    pitPrimitive.Spawn();
+                    break;
+                }
+                case RoomType.Hcz079:
+                {
+                    SpecialWeaponsPrimitive primitive = new(new(-5.40f, -3.5f, -13.5f), room, new(4.5f, 1, 6));
+                    primitive.Spawn();
+                    break;
+                }
             }
         }
     }
