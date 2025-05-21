7b96f095ab81b2a588f6a57d9b8fa1d6030552d5
Lumi <me@jxtq.moe>
2025-05-21 15:23:25 +0100

    feat: better strangle setting

--- SillySCP/API/Features/StruggleSetting.cs
+++ SillySCP/API/Features/StruggleSetting.cs
@@ -24,30 +24,31 @@ namespace SillySCP.API.Features
 
         internal static int SettingId => 836;
 
-        private static readonly DynamicElement Element = new (HintContent, 300);
+        internal static readonly DynamicElement Element = new (HintContent, 300);
 
         public StruggleSetting()
             : base(SettingId, "Struggle", KeyCode.E, hint: "The key bind to press when being strangled by 3114 to potentially break free.")
         {
             _cooldown = new();
+            _percentage = 0;
         }
 
         protected override CustomSetting CreateDuplicate() => new StruggleSetting();
 
         public override CustomHeader Header { get; } = SSSSModule.Header;
 
-        private static readonly Dictionary<Player, (float percentage, Display display)> StranglePercentage = new ();
+        private float _percentage;
 
-        private AbilityCooldown _cooldown;
+        internal Display Display;
+
+        private readonly AbilityCooldown _cooldown;
         
         protected override void HandleSettingUpdate(Player player)
         {
-            if (StranglePercentage.ContainsKey(player) && !player.HasEffect<Strangled>()) Remove(player);
             if (!player.HasEffect<Strangled>() || !_cooldown.IsReady) return;
             _cooldown.Trigger(0.1f);
-            (float percentage, Display display) val = StranglePercentage[player];
-            val.percentage += 5f;
-            if(val.percentage >= 100)
+            _percentage += 5f;
+            if(_percentage >= 100)
             {
                 Exiled.API.Features.Player skeleton = Exiled.API.Features.Player.Get(RoleTypeId.Scp3114).First();
                 Scp3114Role role = (Scp3114Role)skeleton.Role;
@@ -55,36 +56,18 @@ namespace SillySCP.API.Features
                 strangle.SyncTarget = null;
                 strangle._rpcType = Scp3114Strangle.RpcType.AttackInterrupted;
                 strangle.ServerSendRpc(true);
-                Remove(player);
                 skeleton.EnableEffect(EffectType.Disabled, 2, 5);
                 skeleton.PlayShieldBreakSound();
                 return;
             }
-            val.display.Update();
-            StranglePercentage[player] = val;
-        }
-
-        internal static void Add(Player player)
-        {
-            if (StranglePercentage.ContainsKey(player)) return;
-            Display display = new(player.ReferenceHub);
-            StranglePercentage.Add(player, (0, display));
-            display.Elements.Add(Element);
-            display.Update();
-        }
-
-        internal static void Remove(Player player)
-        {
-            if (!StranglePercentage.TryGetValue(player, out var displayInfo)) return;
-            displayInfo.display.Delete();
-            StranglePercentage.Remove(player);
+            Display ??= new(player.ReferenceHub);
+            Display.Update();
         }
 
         private static string HintContent(DisplayCore core)
         {
-            Player player = Player.Get(core.Hub);
-            (float percentage, Display display) val = StranglePercentage[player];
-            return $"{Hint}\n{val.percentage}%";
+            StruggleSetting setting = GetPlayerSetting<StruggleSetting>(SettingId, Player.Get(core.Hub));
+            return setting == null ? "" : $"{Hint}\n{setting._percentage}%";
         }
     }
 }
--- SillySCP/Patches/SkeletonStrangle.cs
+++ SillySCP/Patches/SkeletonStrangle.cs
@@ -1,25 +1,26 @@
-﻿using Exiled.API.Features;
+﻿using CustomPlayerEffects;
+using Exiled.API.Features;
 using HarmonyLib;
 using PlayerRoles.PlayableScps.Scp3114;
+using SecretAPI.Features.UserSettings;
 using SillySCP.API.Features;
+using Utils.NonAllocLINQ;
 using Player = LabApi.Features.Wrappers.Player;
 
 namespace SillySCP.Patches
 {
-    [HarmonyPatch(typeof(Scp3114Strangle), nameof(Scp3114Strangle.SyncTarget), MethodType.Setter)]
+    [HarmonyPatch(typeof(Scp3114Strangle), nameof(Scp3114Strangle.ServerSendRpc))]
     public static class SkeletonStrangle
     {
 #pragma warning disable SA1313
-        private static bool Prefix(Scp3114Strangle __instance, Scp3114Strangle.StrangleTarget? value)
+        private static bool Prefix()
 #pragma warning restore SA1313
         {
-            if (!value.HasValue && !__instance.SyncTarget.HasValue) return true;
-            if(!value.HasValue) {
-                Player player = Player.Get(__instance.SyncTarget.Value.Target);
-                StruggleSetting.Remove(player);
-            } else {
-                Player player = Player.Get(value.Value.Target);
-                StruggleSetting.Add(player);
+            foreach (KeyValuePair<Player, List<CustomSetting>> pair in CustomSetting.PlayerSettings)
+            {
+                if(!pair.Value.TryGetFirst(sett => sett.GetType() == typeof(StruggleSetting), out CustomSetting setting)) continue;
+                if(setting is not StruggleSetting struggle) continue;
+                if(!pair.Key.HasEffect<Strangled>() && struggle.Display.Elements.Contains(StruggleSetting.Element)) struggle.Display.Delete();
             }
 
             return true;
