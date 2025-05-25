c62e4d663daa1426cdbcc28d71b53dd45ced5a82
Lumi <me@jxtq.moe>
2025-05-25 17:28:28 +0100

    fix: people imploding

--- SillySCP/Patches/SnakePatch.cs
+++ SillySCP/Patches/SnakePatch.cs
@@ -1,7 +1,11 @@
-﻿using Exiled.API.Features.Items;
+﻿using System.Reflection.Emit;
+using Exiled.API.Enums;
+using Exiled.API.Features.Items;
+using Exiled.API.Features.Pools;
 using HarmonyLib;
 using InventorySystem.Items.Keycards;
 using InventorySystem.Items.Keycards.Snake;
+using static HarmonyLib.AccessTools;
 using MEC;
 using Mirror;
 
@@ -11,15 +15,35 @@ namespace SillySCP.Patches
     public static class SnakePatch
     {
         // ReSharper disable once InconsistentNaming
-        private static void Postfix(ChaosKeycardItem __instance, NetworkReader reader)
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            if ((ChaosKeycardItem.ChaosMsgType)reader.ReadByte() != ChaosKeycardItem.ChaosMsgType.SnakeMsgSync) return;
-            SnakeNetworkMessage msg = new (reader);
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
+            int index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Ldc_I4_1);
+            newInstructions.InsertRange(
+                index - 1, 
+                [
+                    new (OpCodes.Ldarg_0),
+                    new (OpCodes.Ldloc_1),
+                    new (OpCodes.Call, Method(typeof(SnakePatch), nameof(OnCardUsed))),
+                ]);
+            
+            foreach (CodeInstruction instruction in newInstructions)
+                yield return instruction;
+            
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
+        }
+
+        private static void OnCardUsed(ChaosKeycardItem itemBase, SnakeNetworkMessage msg)
+        {
             if (!msg.HasFlag(SnakeNetworkMessage.SyncFlags.GameOver)) return;
-            Item item = Item.Get(__instance);
+            Item item = Item.Get(itemBase);
             if (item == null) return;
             item.Owner.ShowHint("You failed...");
-            Timing.CallDelayed(3, () => item.Owner.Explode());
+            Timing.CallDelayed(3, () =>
+            {
+                item.Owner.ExplodeEffect(ProjectileType.FragGrenade);
+                item.Owner.Kill("You lost at snake... how could you...");
+            });
         }
     }
 }
