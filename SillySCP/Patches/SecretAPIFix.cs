758bbff2a119f54a680515c72f08afc5a3cda273
Lumi <me@jxtq.moe>
2025-06-22 21:34:47 +0100

    fix: secretapi breaking timers

---
+++ SillySCP/Patches/SecretAPIFix.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using LabApi.Features.Wrappers;
+using SecretAPI.Features.UserSettings;
+using UserSettings.ServerSpecific;
+
+namespace SillySCP.Patches
+{
+    [HarmonyPatch(typeof(ServerSpecificSettingsSync), nameof(ServerSpecificSettingsSync.SendToPlayer), new Type[] { typeof(ReferenceHub) })]
+    public static class SecretAPIFix
+    {
+        private static bool Prefix(ReferenceHub hub)
+        {
+            CustomSetting.SendSettingsToPlayer(Player.Get(hub));
+            return false;
+        }
+    }
+}
