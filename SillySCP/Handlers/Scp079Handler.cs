fdc00a158fff917058142a98a5892021f367eca5
Lumi <me@jxtq.moe>
2025-09-03 22:14:37 +0100

    fix: 079 lcz armoury opening

---
+++ SillySCP/Handlers/Scp079Handler.cs
@@ -0,0 +1,42 @@
+using LabApi.Events.Arguments.PlayerEvents;
+using LabApi.Events.Handlers;
+using LabApi.Features.Enums;
+using LabApi.Features.Wrappers;
+using MapGeneration;
+using PlayerRoles;
+using SecretAPI.Features;
+using SillySCP.API.Features;
+
+namespace SillySCP.Handlers;
+
+public class Scp079Handler : IRegister
+{
+    public void TryRegister()
+    {
+        PlayerEvents.InteractingDoor += OnOpenDoor;
+    }
+
+    public void TryUnregister()
+    {
+        PlayerEvents.InteractingDoor -= OnOpenDoor;
+    }
+
+    public static void OnOpenDoor(PlayerInteractingDoorEventArgs ev)
+    {
+        if (ev.Player.Role != RoleTypeId.Scp079 && ev.Door.DoorName != DoorName.LczArmory)
+            return;
+        
+        if (ev.Door.IsOpened)
+            return;
+
+        Room room = ev.Door.Rooms.FirstOrDefault(r => r.Name == RoomName.LczArmory);
+
+        if (room == null)
+            return;
+
+        if (!VolunteerSystem.ReadyVolunteers)
+            return;
+
+        ev.CanOpen = false;
+    }
+}
