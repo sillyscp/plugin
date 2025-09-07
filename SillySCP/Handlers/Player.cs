cbfafe319963d7257aa0b56851df3ed163a6c9dc
Lumi <me@jxtq.moe>
2025-09-07 01:01:04 +0100

    fix: bee movie duplication

--- SillySCP/Handlers/Player.cs
+++ SillySCP/Handlers/Player.cs
@@ -94,7 +94,7 @@ namespace SillySCP.Handlers
                 "https://gist.githubusercontent.com/MattIPv4/045239bc27b16b2bcf7a3a9a4648c08a/raw/2411e31293a35f3e565f61e7490a806d4720ea7e/bee%2520movie%2520script");
 
             BeeMovieScript = await message.Content.ReadAsStringAsync();
-            BeeMovieScript = BeeMovieScript.Replace("\n", " - ").Substring(0, 10000);
+            BeeMovieScript = BeeMovieScript.Replace("\n", " - ").Substring(0, 10000).Trim();
         }
 
         private static void OnSpawningRagdoll(PlayerSpawningRagdollEventArgs ev)
@@ -104,7 +104,7 @@ namespace SillySCP.Handlers
             
             bool anyRagdoll = Ragdoll.List.Any(ragdoll =>
                 ragdoll.DamageHandler is CustomReasonDamageHandler handler &&
-                handler.RagdollInspectText == BeeMovieScript);
+                handler._deathReason.Trim().Contains(BeeMovieScript));
             
             if (anyRagdoll && Random.Range(0, 25) != 0) return;
 
