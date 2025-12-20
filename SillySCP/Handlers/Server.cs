8e5370b570f80c2597d0ae29409222eab0a9b3eb
Lumi <me@jxtq.moe>
2025-12-20 22:06:43 +0000

    feat: random silly christmas things

--- SillySCP/Handlers/Server.cs
+++ SillySCP/Handlers/Server.cs
@@ -4,34 +4,39 @@ using LabApi.Events.Arguments.Scp914Events;
 using LabApi.Events.Arguments.ServerEvents;
 using LabApi.Events.Handlers;
 using LabApi.Features.Wrappers;
+using MapGeneration.Holidays;
 using MEC;
+using PlayerRoles;
 using Scp914;
+using SecretAPI.Extensions;
 using SecretAPI.Features;
 using SillySCP.API.Features;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace SillySCP.Handlers
 {
     public class Server : IRegister
     {
         public static Server Instance { get; private set; }
-        
+
         public HttpClient Client { get; private set; }
-        
+
         public void TryRegister()
         {
             Instance = this;
             Client = new();
-            Client.BaseAddress = new (Plugin.Instance.Config!.WebhookUrl);
+            Client.BaseAddress = new(Plugin.Instance.Config!.WebhookUrl);
             ServerEvents.WaitingForPlayers += OnWaitingForPlayers;
             ServerEvents.RoundStarted += OnRoundStarted;
             ServerEvents.RoundRestarted += OnRoundRestart;
             Scp914Events.ProcessingPickup += OnScp914UpgradePickup;
             ServerEvents.CassieQueuingScpTermination += OnAnnouncingScpTermination;
-            
+            ServerEvents.WaveRespawned += OnWaveRespawned;
+
             Application.logMessageReceived += OnLogReceived;
         }
-        
+
         public void TryUnregister()
         {
             Client.Dispose();
@@ -41,18 +46,45 @@ namespace SillySCP.Handlers
             ServerEvents.RoundRestarted -= OnRoundRestart;
             Scp914Events.ProcessingPickup -= OnScp914UpgradePickup;
             ServerEvents.CassieQueuingScpTermination -= OnAnnouncingScpTermination;
-            
+            ServerEvents.WaveRespawned -= OnWaveRespawned;
+
             Application.logMessageReceived -= OnLogReceived;
+        }
+
+        private void OnWaveRespawned(WaveRespawnedEventArgs ev)
+        {
+            if (HolidayUtils.GetActiveHoliday() != HolidayType.Christmas)
+                return;
+
+            if (ev.Wave.Faction is Faction.Flamingos or Faction.SCP)
+                return;
+
+            if (Random.Range(0, 100) < 1 && ev.Wave is MiniMtfWave or MiniChaosWave)
+            {
+                foreach (LabApi.Features.Wrappers.Player player in ev.Players)
+                {
+                    player.Role = ev.Wave.Faction == Faction.FoundationEnemy
+                        ? RoleTypeId.ChaosFlamingo
+                        : RoleTypeId.NtfFlamingo;
+                }
+            }
+            else
+            {
+                ev.Players.GetRandomValue().Role = ev.Wave.Faction == Faction.FoundationEnemy
+                    ? RoleTypeId.ChaosFlamingo
+                    : RoleTypeId.NtfFlamingo;
+            }
         }
 
         private void OnLogReceived(string logString, string stackTrace, LogType type)
         {
             if (!logString.StartsWith("Disconnecting connId=")) return;
-            
-            logString = string.Join("\n", logString.Split('\n').Take(2)).Replace("\\", @"\\").Replace("\"", "\\\"").Replace("\r", "\\r").Replace("\n", "\\n");
-            
+
+            logString = string.Join("\n", logString.Split('\n').Take(2)).Replace("\\", @"\\").Replace("\"", "\\\"")
+                .Replace("\r", "\\r").Replace("\n", "\\n");
+
             string jsonString = $"{{\"content\":\"{logString}\"}}";
-            
+
             StringContent content = new(jsonString, Encoding.UTF8, "application/json");
 
             Client.PostAsync("", content);
@@ -65,12 +97,12 @@ namespace SillySCP.Handlers
                 ev.IsAllowed = false;
             }
         }
-        
+
         private void OnWaitingForPlayers()
         {
             Plugin.Instance.Scp106 = null;
         }
-        
+
         private void OnRoundStarted()
         {
             LabApi.Features.Wrappers.Server.FriendlyFire = false;
@@ -82,7 +114,7 @@ namespace SillySCP.Handlers
         {
             LabApi.Features.Wrappers.Server.FriendlyFire = false;
         }
-        
+
         private void OnScp914UpgradePickup(Scp914ProcessingPickupEventArgs ev)
         {
             if (ev.KnobSetting == Scp914KnobSetting.Fine && ev.Pickup.Type == ItemType.Coin)
